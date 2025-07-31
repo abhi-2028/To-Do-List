d2dbb7939978d7f352c49351fb864f138f363131
Abhirup Biswas <abhirupbiswas1clg@gmail.com>
2025-07-31 09:50:50 +0530

    added the press enter to add task logic

--- script.js
+++ script.js
@@ -19,6 +19,25 @@ addTask.addEventListener("click", function () {
     }
 });
 
+task.addEventListener("keypress", function (event) {
+    if (event.key == "Enter") {
+        let item = document.createElement("li");
+        item.innerText = task.value;
+
+        let dltBtn = document.createElement("button");
+        dltBtn.innerText = "delete";
+        dltBtn.classList.add("delete");
+
+        if (item.innerText != "") {
+            list.appendChild(item);
+            item.appendChild(dltBtn);
+            task.value = "";
+        } else {
+            alert("Enter task");
+        }
+    }
+})
+
 list.addEventListener("click", function (event) {
     if (event.target.nodeName == "BUTTON") {
         let listItem = event.target.parentElement;
